460a71c0f2f306cc6e4c96df63f91152dc3add2a
aWuttig <wuttig@portrino.de>
2016-09-02 17:09:59 +0200

    [FEATURE] Loads typo3ApiUrl from current logged in backend user

--- Service/Notification/NotificationService.php
+++ Service/Notification/NotificationService.php
@@ -17,6 +17,44 @@ namespace Port1Typo3Connector\Service\Notification;
 class NotificationService
 {
     /**
+     * @var \Shopware\Models\User\Repository
+     */
+    protected $userRepository = null;
+
+    /**
+     * @var string
+     */
+    protected $typo3ApiUrl = '';
+
+    /**
+     * NotificationService constructor.
+     */
+    public function __construct()
+    {
+        $id = Shopware()->Container()->get('auth')->getIdentity()->id;
+
+        /** @var array|null $data */
+        $data = $this->getUserRepository()
+                     ->getAttributesQuery($id)
+                     ->getOneOrNullResult(\Doctrine\ORM\AbstractQuery::HYDRATE_ARRAY);
+
+        $this->typo3ApiUrl = trim($data['typo3ApiUrl']);
+    }
+
+
+    /**
+     * Helper function to get access to the user repository.
+     * @return \Shopware\Models\User\Repository
+     */
+    private function getUserRepository()
+    {
+        if ($this->userRepository === null) {
+            $this->userRepository = Shopware()->Models()->getRepository('Shopware\Models\User\User');
+        }
+        return $this->userRepository;
+    }
+
+    /**
      * sends the notification to TYPO3
      *
      * @param $action
